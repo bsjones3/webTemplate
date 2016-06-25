d46ec8cb465211033101057357a9e4379dbdc2b7
bsjones3 <brandonjones@Brandon-Joness-MacBook-Pro.local>
2016-06-24 23:33:03 -0400

    creating baseless
    adding a homepage index.php and a page to process form input

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>web template</title>
+	<link rel="stylesheet" href="styles.css"/>
+</head>
+<body>
+	<form action="userAccept.php" method="POST">
+		Username:
+		<input type="text" name="usern" value="Enter User"><br>
+
+		Password:
+		<input type="password" name="passw" value="Enter Pass"><br>
+		<input type="submit" value="submit">
+	</form>
+</body>
+</html>
